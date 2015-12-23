f2a14a0d558b63ec59355f6d515d9e9864b6d003
Archie <yangyxcn@gmail.com>
2015-12-23 23:33:28 +0800

    Implement full screen and normal screen switch
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- app/src/main/java/me/codethink/ijkplayerdemo/MainActivity.java
+++ app/src/main/java/me/codethink/ijkplayerdemo/MainActivity.java
@@ -2,61 +2,77 @@ package me.codethink.ijkplayerdemo;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.MediaController;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     IjkVideoView mIjkVideoView = null;
-    FrameLayout container = null;
+    LinearLayout container = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        container = (FrameLayout) findViewById(R.id.container);
+        container = (LinearLayout) findViewById(R.id.container);
 
         mIjkVideoView = (IjkVideoView)findViewById(R.id.video_view);
         mIjkVideoView.setRender(IjkVideoView.RENDER_TEXTURE_VIEW);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.video_controller__screen_size).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//                mIjkVideoView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-//                    mIjkVideoView.setRotation(90);
-//                container.setRotation(90);
+                findViewById(R.id.tv).setVisibility(View.GONE);
+                ViewGroup.LayoutParams layoutParams = ((FrameLayout)findViewById(R.id.con2)).getLayoutParams();
+
+                layoutParams.height = container.getWidth();
+                findViewById(R.id.con2).requestLayout();
 
 
-//                int w = container.getWidth();
-//                int h = container.getHeight();
-//
-//                container.setRotation(270.0f);
-//                container.setTranslationX((w - h) / 2);
-//                container.setTranslationY((h - w) / 2);
-//
-//                ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) container.getLayoutParams();
-//                lp.height = w;
-//                lp.width = h;
-//                container.requestLayout();
-                mIjkVideoView.toggleFullscreen();
+                int w = container.getWidth();
+                int h = container.getHeight();
 
+                container.setRotation(90);
+                container.setTranslationX((w - h) / 2);
+                container.setTranslationY((h - w) / 2);
+
+                ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) container.getLayoutParams();
+                lp.height = w;
+                lp.width = h;
+                container.requestLayout();
 
             }
         });
 
+        findViewById(R.id.player_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.tv).setVisibility(View.VISIBLE);
+                ViewGroup.LayoutParams layoutParams = ((FrameLayout)findViewById(R.id.con2)).getLayoutParams();
+                layoutParams.height = 609;
+                findViewById(R.id.con2).requestLayout();
 
+
+                int w = container.getWidth();
+                int h = container.getHeight();
+
+                container.setRotation(0);
+                container.setTranslationX(0);
+                container.setTranslationY(0);
+
+                ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) container.getLayoutParams();
+                lp.height = w;
+                lp.width = h;
+                container.requestLayout();
+
+            }
+        });
 
         mIjkVideoView.setVideoURI(Uri.parse("http://7xli8r.com5.z0.glb.qiniucdn.com/zuiehehee11000k"));
         mIjkVideoView.start();
